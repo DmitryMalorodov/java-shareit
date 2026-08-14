1297deffac479bcb1080315bb1c8ee46d708cea1
Малородов Дмитрий Николаевич <malorodovd@yandex.ru>
2026-08-14 17:41:00 +0300

    - удавление лишних методов перехватчиков исключений у сервера

--- server/src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/handler/ErrorHandler.java
@@ -1,19 +1,13 @@
 package ru.practicum.shareit.handler;
 
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import ru.practicum.shareit.exceptions.AccessDeniedException;
 import ru.practicum.shareit.exceptions.NotFoundException;
 import ru.practicum.shareit.exceptions.ValidationException;
-
-import java.util.Arrays;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -35,42 +29,10 @@ public class ErrorHandler {
         return new ErrorResponse(e.getMessage());
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMethodArgumentNotValid(final MethodArgumentNotValidException e) {
-        String message = e.getBindingResult().getAllErrors().stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .findFirst()
-                .orElse("Ошибка валидации полей объекта");
-        return new ErrorResponse(message);
-    }
-
-    @ExceptionHandler(MissingRequestHeaderException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMissingRequestHeader(final MissingRequestHeaderException e) {
-        return new ErrorResponse("Заголовок X-Sharer-User-Id не передан или некорректен");
-    }
-
     @ExceptionHandler(DataIntegrityViolationException.class)
     @ResponseStatus(HttpStatus.CONFLICT) // Возвращает статус 409
     public ErrorResponse handleConflictException(final DataIntegrityViolationException e) {
         return new ErrorResponse(e.getMessage());
-    }
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMethodArgumentTypeMismatchException(final MethodArgumentTypeMismatchException e) {
-        String parameterName = e.getName();
-        String providedValue = String.valueOf(e.getValue());
-
-        if (e.getRequiredType() != null && e.getRequiredType().isEnum()) {
-            String message = String.format("Значение '%s' неверно для параметра '%s'. Допустимые значения: %s",
-                    providedValue, parameterName, Arrays.toString(e.getRequiredType().getEnumConstants()));
-
-            return new ErrorResponse(message);
-        }
-
-        return new ErrorResponse("Неверный тип параметра");
     }
 
     @ExceptionHandler
